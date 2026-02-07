45a6b410451b6f08f81b2e6c32775bd1bfd963e3
Apoorv Jain <apoorvjainji@gmail.com>
2026-02-07 10:22:13 +0530

    New Problems

--- Array and Matrix Manipulations/Type1.java
+++ Array and Matrix Manipulations/Type1.java
@@ -33,4 +33,36 @@ public class Type1 {
         System.out.println(Arrays.toString(matrix[i]));
 }
     }
+    // Spiral Matrix LeetCode- 54
+    public List<Integer> spiralOrder(int[][] matrix) {
+         List<Integer> result = new ArrayList<>();
+        if(matrix.length == 0) return result;
+        int m= matrix.length, n= matrix[0].length;
+        int startRow=0, endRow=m-1, startCol=0, endCol=n-1;
+        while(startRow<=endRow && startCol<=endCol){
+            for(int i=startCol; i<=endCol; i++){
+                result.add(matrix[startRow][i]);
+            }
+            startRow++;
+            for(int i=startRow; i<=endRow; i++){
+                result.add(matrix[i][endCol]);
+            }
+            endCol--;
+            if(startRow <= endRow){
+                for(int i=endCol; i>=startCol; i--){
+                    result.add(matrix[endRow][i]);
+                }
+                endRow--;
+            }
+            if(startCol <= endCol){
+                for(int i=endRow; i>=startRow; i--){
+                    result.add(matrix[i][startCol]);
+                }
+                startCol++;
+            }
+
+        }
+        return result;
+    }
+    
 }
